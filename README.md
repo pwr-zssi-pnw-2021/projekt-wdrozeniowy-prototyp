8e77629cdfbec64d4da4b94ea164e563b211770f
Jakub Ciszek <jakubciszek64@gmail.com>
2021-10-06 15:56:01 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projekt Naukowo-Wdrożeniowy
+
+## Prototyp
+
+## Temat: Real Time Speech Emotion Recognition
---
+++ tests/test_projekt_wdrozeniowy_prototyp.py
@@ -0,0 +1,5 @@
+from projekt_wdrozeniowy_prototyp import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'

8f5f3940f6b216ab8f5129293304ae19da5f84b4
Jakub Ciszek <jakubciszek64@gmail.com>
2022-01-23 02:47:01 +0100

    Array source tests

--- tests/test_generator_source.py
+++ tests/test_generator_source.py
@@ -1,6 +1,11 @@
+import librosa
+import numpy as np
 import pytest
 
-from projekt_wdrozeniowy_prototyp.sound_generator import FileSource
+from projekt_wdrozeniowy_prototyp.sound_generator import (
+    ArraySource,
+    FileSource,
+)
 
 
 def test_file_loading():
@@ -20,3 +25,23 @@ def test_draw():
     v = next(iter(fs.draw()))
 
     assert v is not None
+
+
+def test_array_source():
+    array = np.arange(9)
+    arr_source = ArraySource(array, 42)
+
+    loaded_array = np.array(list(arr_source.draw()))
+    assert np.array_equal(array, loaded_array)
+
+
+def test_array_source_resample():
+    array = np.arange(9).astype(np.float32)
+    initial_sr = 42
+    target_sr = 23
+
+    arr_source = ArraySource(array, initial_sr, target_sr)
+    resampled_array = librosa.resample(array, initial_sr, target_sr)
+    loaded_array = np.array(list(arr_source.draw()))
+
+    assert np.array_equal(resampled_array, loaded_array)

1268e36389e563d0150e2d7fac4e56284926e40a
Jakub Ciszek <jakubciszek64@gmail.com>
2022-01-23 02:26:27 +0100

    Streamlit interface

---
+++ projekt_wdrozeniowy_prototyp/interface.py
@@ -0,0 +1,43 @@
+import librosa
+import numpy as np
+import streamlit as st
+
+from projekt_wdrozeniowy_prototyp.sound_generator import (
+    ArraySource,
+    SoundGenerator,
+)
+
+EMOTIONS = [
+    'Anger',
+    'Disgust',
+    'Fear',
+    'Happiness',
+    'Neutral',
+    'Sadness',
+    'Surprise',
+]
+
+st.title('Real Time Speech Emotion Recognition')
+
+
+uploaded_file = st.file_uploader('Select audio file')
+if uploaded_file is not None:
+    st.caption('Press play to play audio.')
+    st.audio(uploaded_file)
+
+    data, sr = librosa.load(uploaded_file, sr=48000)
+    source = ArraySource(data, sr)
+    generator = SoundGenerator(source)
+
+    st.caption('Press analyze to analyze emotions in audio.')
+    if st.button('Analyze'):
+        placeholder = st.empty()
+        for v in generator.generate():
+            # TODO process data
+            prob = np.random.uniform(0, 1, 7)
+
+            with placeholder.container():
+                for emotion, value in zip(EMOTIONS, prob):
+                    st.text(
+                        f'{emotion:10}: {value:.3f} | {round(value * 10 * 4) * "#"}'
+                    )
--- projekt_wdrozeniowy_prototyp/sound_generator/__init__.py
+++ projekt_wdrozeniowy_prototyp/sound_generator/__init__.py
@@ -1,10 +1,11 @@
 from .preprocessor import MFCCPreprocessor, RawPreprocessor
 from .sound_generator import SoundGenerator
-from .source import FileSource
+from .source import ArraySource, FileSource
 
 __all__ = [
     'MFCCPreprocessor',
     'RawPreprocessor',
     'SoundGenerator',
     'FileSource',
+    'ArraySource',
 ]

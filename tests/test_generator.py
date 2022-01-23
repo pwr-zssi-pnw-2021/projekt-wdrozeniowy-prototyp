a6f377f7d0a94dc39ffe613b91a70246c80b6728
Jakub Ciszek <jakubciszek64@gmail.com>
2022-01-23 03:21:25 +0100

    Generator time sync test, interface ex

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,18 +1,34 @@
+import time
+
 import numpy as np
 import pytest
 
 from projekt_wdrozeniowy_prototyp.sound_generator import (
+    ArraySource,
     FileSource,
     RawPreprocessor,
     SoundGenerator,
 )
 
 
-@pytest.fixture()
-def source():
-    fs = FileSource('./tests/data/CantinaBand3.wav', 48000)
-    fs.data_source = np.arange(64)
-    yield fs
+@pytest.fixture
+def sampling_rate() -> int:
+    return 48000
+
+
+@pytest.fixture
+def cantina_band_3s_source(sampling_rate: int) -> FileSource:
+    source = FileSource('./tests/data/CantinaBand3.wav', sampling_rate)
+
+    return source
+
+
+@pytest.fixture
+def array_source(sampling_rate: int) -> ArraySource:
+    arr = np.arange(64)
+    source = ArraySource(arr, sampling_rate)
+
+    return source
 
 
 @pytest.mark.parametrize(
@@ -23,8 +39,8 @@ def source():
         (9,),
     ],
 )
-def test_chunk_size(source: FileSource, chunk_size: int):
-    generator = SoundGenerator(source, RawPreprocessor(), chunk_size, 0)
+def test_chunk_size(array_source: ArraySource, chunk_size: int):
+    generator = SoundGenerator(array_source, RawPreprocessor(), chunk_size, 0)
 
     chunk = next(iter(generator.generate()))
 
@@ -39,8 +55,8 @@ def test_chunk_size(source: FileSource, chunk_size: int):
         (5,),
     ],
 )
-def test_shift(source: FileSource, shift: int):
-    generator = SoundGenerator(source, RawPreprocessor(), 8, shift)
+def test_shift(array_source: ArraySource, shift: int):
+    generator = SoundGenerator(array_source, RawPreprocessor(), 8, shift)
     gen_it = iter(generator.generate())
 
     chunk1 = next(gen_it)
@@ -49,7 +65,32 @@ def test_shift(source: FileSource, shift: int):
     assert chunk1[1 + shift] == chunk2[0]
 
 
-def test_default_preprocessor(source: FileSource):
-    generator = SoundGenerator(source)
+def test_default_preprocessor(array_source: ArraySource):
+    generator = SoundGenerator(array_source)
 
     assert isinstance(generator.preprocessor, RawPreprocessor)
+
+
+def test_generating_time(cantina_band_3s_source: FileSource):
+    generator = SoundGenerator(cantina_band_3s_source, chunk_size=2048, shift=1024)
+    target_time = 3
+
+    time_start = time.perf_counter()
+    for _ in generator.generate():
+        pass
+    time_end = time.perf_counter()
+
+    assert abs((time_end - time_start) - target_time) < 10e-1
+
+
+def test_generating_compensation(cantina_band_3s_source: FileSource):
+    generator = SoundGenerator(cantina_band_3s_source, chunk_size=2048, shift=1024)
+    target_time = 3
+
+    time_start = time.perf_counter()
+    for i, _ in enumerate(generator.generate()):
+        if i == 2:
+            time.sleep(0.5)
+    time_end = time.perf_counter()
+
+    assert abs((time_end - time_start) - target_time) < 10e-1
